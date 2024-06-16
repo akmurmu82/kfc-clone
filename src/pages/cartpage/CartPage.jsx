4c12ad27cca4b1e47d5bea2a545a0a8f94347401
amit murmu <your.akmurmu6580@google.com>
2024-06-16 10:39:42 +0530

    created cartArr for cartpage

--- src/pages/cartpage/CartPage.jsx
+++ src/pages/cartpage/CartPage.jsx
@@ -1,15 +1,55 @@
 import { Box, Flex, Text, VStack } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import CartProduct from "../../components/cartpage/CartProduct";
+import { fetchProductDetails } from "../../services/cartServices";
+import { useEffect, useState } from "react";
 
 function CartPage() {
-  return <Box>
-    <Text>CART</Text>
-    <Flex>
-        <VStack>
+  const cart = useSelector((state) => state.cart.items);
+  const [cartArr, setCartArr] = useState([]);
+  console.log(cart);
 
+  async function getCartDetails(cart) {
+    const detailedCartItems = await Promise.all(
+      cart.map(async (item) => {
+        const productDetails = await fetchProductDetails(item.productId);
+        let product = productDetails[0]; // maa chud gyi isko discover karte karte
+        return {
+          ...item,
+          ...product,
+        };
+      })
+    );
+    return detailedCartItems;
+  }
+
+  useEffect(() => {
+    const genCartDetails = async () => {
+      const cartArr = await getCartDetails(cart);
+      console.log("cartArr:", cartArr);
+      setCartArr(cartArr);
+    };
+    genCartDetails();
+  }, []);
+
+  return (
+    <Box>
+      <Text>CART</Text>
+      <Flex>
+        <VStack>
+          {cartArr.map(({ image, name, quantity, price }, index) => (
+            <CartProduct
+              key={index}
+              name={name}
+              image={image}
+              price={price}
+              quantity={quantity}
+            />
+          ))}
         </VStack>
-        
-    </Flex>
-  </Box>;
+      </Flex>
+    </Box>
+  );
 }
 
 export default CartPage;
--- src/services/cartServices.js
+++ src/services/cartServices.js
@@ -3,8 +3,7 @@ import BE_BASE_URL from "../config";
 
 export const addToCart = async (userId, productId, quantity, price) => {
   try {
-      
-  const response = await axios.post(`${BE_BASE_URL}/cart/add`, {
+    const response = await axios.post(`${BE_BASE_URL}/cart/add`, {
       userId,
       productId,
       quantity,
@@ -16,6 +15,7 @@ export const addToCart = async (userId, productId, quantity, price) => {
     throw error;
   }
 };
+
 export const getCart = async (userId) => {
   try {
     const response = await axios.post(`${BE_BASE_URL}/cart/get`, {
@@ -35,4 +35,10 @@ export const fetchProducts = async () => {
   } catch (error) {
     console.error("Failed to fetch products", error);
   }
+};
+
+export const fetchProductDetails = async (productId) => {
+  let res = await axios.post(`${BE_BASE_URL}/products/${productId}`);
+  // console.log("product:", typeof res.data.data, res.data.data);
+  return res.data.data;
 };
